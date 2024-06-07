43e5f2a622f2e928bb5010533550149c80bd9beb
Jawahar <jawaharbhaskaran@gmail.com>
2024-06-07 11:13:31 +0530

    Added: Docs Implementation of basic Table Structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { WjGridModule, WjFlexGrid } from '@mescius/wijmo.angular2.grid';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'wijmodemojb';
+  data: any[]
+  @ViewChild('flex') flex: WjFlexGrid | undefined;
+
+  constructor(){
+    this.data = [
+      { id: 0, country: "US", sales: 1318.37, expenses: 4212.41 },
+      { id: 1, country: "Germany", sales: 5847.95, expenses: 89.79 },
+      { id: 2, country: "UK", sales: 502.55, expenses: 2878.50 },
+      { id: 3, country: "Japan", sales: 4675.40, expenses: 488.65 },
+      { id: 4, country: "Italy", sales: 2117.57, expenses: 925.60 },
+      { id: 5, country: "Greece", sales: 322.10, expenses: 4163.96 }
+  ];
+  }
 }
